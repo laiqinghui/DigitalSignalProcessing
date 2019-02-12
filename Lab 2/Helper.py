8f92116684cd7d9e0c7c9b6564875b7122227e3b
Qinghui <qinghui1994@gmail.com>
2019-02-12 17:24:36 +0800

    Lab 2 q 3 4 5

--- Lab 2/Helper.py
+++ Lab 2/Helper.py
@@ -8,7 +8,7 @@ class Helper():
 
     # The following function generates a continuous time sinusoid
     # given the amplitude A, F (cycles/seconds), Fs=sampling rate, start and endtime
-    def fnGenSampledSinusoid(self, A, Freq,Phi, Fs, sTime, eTime):
+    def fnGenSampledSinusoid(self, A, Freq, Phi, Fs, sTime, eTime):
         # Showing off how to use numerical python library to create arange
         n = np.arange(sTime,eTime,1.0/Fs)
         y = A*np.cos(2 * np.pi * Freq * n + Phi)
@@ -27,26 +27,24 @@ class Helper():
 
     def convolve(self, x, h):
 
+        # xxxxxxx
+        # 0xxxxxxxxx0
+        #
+        #          xxxxxxx
+        # 0xxxxxxxxx000000
+        lenXOrig = len(x)
+
         y = []
         #1) flip h
         h = h[::-1]
         #print "Flipped h:", h
         #2) append 0 to front of x and end of x
-        x = np.concatenate(([0], x, [0]))
-        #print "Modded x:", x
-        print "len of x-1: ", len(x)-1
-        for i in range(len(x)-1):
-            limit = i + len(h)
+        for i in range(len(h)-1):
+            x = np.concatenate(([0], x, [0]))
 
-            if len(x[i:limit]) < len(h):
-                # print "current i", i
-                # print "limit", limit
-                # print "len of x[i:limit]: ", len(x[i:limit])
-                # print "range(len(h) - len(x[i:limit]): ", range(len(h) - len(x[i:limit]))
-                for e in range((len(h) - len(x[i:limit]))+1):
-                    x = np.concatenate((x, [0]))
+        for i in range(lenXOrig + len(h) - 1):
+            limit = i + len(h)
             sum = np.sum(np.multiply(h, x[i:limit]))
-            #print sum
             y.append(sum)
 
         return np.array(y)
--- Lab 2/Main.py
+++ Lab 2/Main.py
@@ -46,9 +46,10 @@ def q3():
     print "Convolving..."
     y = np.convolve(sampleX_float, impulseH)
     y_self = helper.convolve(sampleX_float, impulseH)
+    print "len of sampleX_float: ", len(sampleX_float)
     print y
     print y_self
-    y_16bit = helper.fnNormalizeFloatTo16Bit(y)
+    y_16bit = helper.fnNormalizeFloatTo16Bit(y_self)
     wavfile.write('t1_16bit.wav', Fs, y_16bit)
     print "Playing convolved wav file..."
     winsound.PlaySound('t1_16bit.wav', winsound.SND_FILENAME)
@@ -62,12 +63,127 @@ def q3():
 
 def q3_self():
 
-    x = np.array([1,2,3])
-    h = np.array([1,2])
+    x = np.array([1,2,3, 4, 5, 6, 7, 8, 9])
+    h = np.array([1,2, 3, 4, 5, 6])
 
     y1 = helper.convolve(x, h)
     y2 = np.convolve(x,h)
     print y1
     print y2
 
-q3()
+
+def delta(n):
+
+    if n == 0:
+        return 1
+    else: return 0
+
+def q4():
+
+    n = np.arange(0, 100)
+    h1 = np.array([0.06523, 0.14936, 0.21529, 0.2402, 0.21529, 0.14936, 0.06523], dtype='float')
+    h2 = np.array([-0.06523, -0.14936, -0.21529, 0.7598, -0.21529, -0.14936, -0.06523], dtype='float')
+
+    x1 = np.zeros(len(n))
+    for i in range(len(n)):
+        x1[i] = delta(n[i]) - 2*delta(n[i]-15)
+    y1 = helper.convolve(x1, h1)
+    y2 = helper.convolve(x1, h2)
+
+    x2n, x2 = helper.fnGenSampledSinusoid(0.1, 700, 0, 16000, 0, 0.05)
+    x3 = np.zeros(len(n))
+    x3n, x3 = helper.fnGenSampledSinusoid(0.1, 3333, 0, 16000, 0, 0.05)
+    x4 = x2 + x3
+    y3 = helper.convolve(x4, h1)
+    y4 = helper.convolve(x4, h2)
+    #print "x4: ", x4
+    # x4float = helper.fnNormalize16BitToFloat(x4)
+    # [f, t, Sxx] = signal.spectrogram(x4float, 16000, window=('blackmanharris'),nperseg=512,noverlap=int(0.9*512))
+    # plt.pcolormesh(t, f, 10*np.log10(Sxx))
+    # plt.ylabel('Frequency [Hz]')
+    # plt.xlabel('Time [sec]')
+    # plt.title('spectrogram of signal')
+    # plt.show()
+
+    fig , axes = plt.subplots(5,1)
+    axes[0].plot(x2n, x2)
+    axes[0].grid()
+    axes[1].plot(x3n, x3)
+    axes[1].grid()
+    axes[2].plot(x3n, x4)
+    axes[2].grid()
+    axes[3].plot(y3)
+    axes[3].grid()
+    axes[4].plot(y4)
+    axes[4].grid()
+    plt.show()
+
+    # x2_16bit = helper.fnNormalizeFloatTo16Bit(x2)
+    # wavfile.write('t1_16bit.wav', 16000, x2_16bit)
+    # print "Playing x2_16bit wav file..."
+    # winsound.PlaySound('t1_16bit.wav', winsound.SND_FILENAME)
+    # x3_16bit = helper.fnNormalizeFloatTo16Bit(x3)
+    # wavfile.write('t1_16bit.wav', 16000, x3_16bit)
+    # print "Playing x3_16bit wav file..."
+    # winsound.PlaySound('t1_16bit.wav', winsound.SND_FILENAME)
+    # x4_16bit = helper.fnNormalizeFloatTo16Bit(x4)
+    # wavfile.write('t1_16bit.wav', 16000, x4_16bit)
+    # print "Playing x4_16bit wav file..."
+    # winsound.PlaySound('t1_16bit.wav', winsound.SND_FILENAME)
+    # y3_16bit = helper.fnNormalizeFloatTo16Bit(y3)
+    # wavfile.write('t1_16bit.wav', 16000, y3_16bit)
+    # print "Playing y3_16bit wav file..."
+    # winsound.PlaySound('t1_16bit.wav', winsound.SND_FILENAME)
+    # y4_16bit = helper.fnNormalizeFloatTo16Bit(y4)
+    # wavfile.write('t1_16bit.wav', 16000, y4_16bit)
+    # print "Playing y4_16bit wav file..."
+    # winsound.PlaySound('t1_16bit.wav', winsound.SND_FILENAME)
+
+def q5():
+
+    # B = [1, -0.7653668, 0.99999], A = [1, -0.722744, 0.888622]
+
+    ipcleanfilename = 'helloworld_noisy_16bit.wav'
+    print "Playing original noisy wav file..."
+    winsound.PlaySound(ipcleanfilename, winsound.SND_FILENAME)
+    Fs, sampleX_16bit = wavfile.read(ipcleanfilename)
+    x = helper.fnNormalize16BitToFloat(sampleX_16bit)
+    y = np.zeros(len(x), dtype=float)
+
+    for n in range(len(x)):
+        if n == 0:
+            y[n] = 1*x[n]
+        elif n == 1:
+            y[n] = 1*x[n] + (-0.7653668)*x[n-1] - (-0.722744)*y[n-1]
+        elif n == 2:
+            y[n] = 1*x[n] + (-0.7653668)*x[n-1] + 0.99999*x[n-2] - (-0.722744)*y[n-1]
+        else:
+            #print "Current n: ", n
+            y[n] = 1*x[n] + (-0.7653668)*x[n-1] + 0.99999*x[n-2] - (-0.722744)*y[n-1] - 0.888622*y[n-3]
+
+    B = [1, -0.7653668, 0.99999]
+    A = [1, -0.722744, 0.888622]
+    y_ifil = signal.lfilter(B, A, x)
+
+    for n in range(len(y[0:10])):
+
+        print y[n]
+        print y_ifil[n]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+q5()
